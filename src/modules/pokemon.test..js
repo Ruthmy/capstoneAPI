f801eb6229f86ab6b6769c2847a4abfd0202f54d
Khaled-Alaa-1 <117099184+Khaled-Alaa-1@users.noreply.github.com>
2023-05-07 07:52:56 +0200

    Create pokemon.test..js

---
+++ src/modules/pokemon.test..js
@@ -0,0 +1,17 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { amountOfComments } from './pokemon.js';
+
+describe('amountOfComments', () => {
+  test('should update the span element with the correct number of comments', () => {
+    const pokemonID = 'pk-1';
+    const totalComments = '3';
+    document.body.innerHTML = '<span class="amount-pk-1"></span>';
+    amountOfComments(totalComments, pokemonID);
+    const spanElement = document.querySelector('.amount-pk-1');
+    expect(spanElement.textContent).toBe('3');
+  });
+});
+
